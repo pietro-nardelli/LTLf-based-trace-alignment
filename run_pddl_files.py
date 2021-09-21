e413146a1c91afd2453f8794b60cd95fabb1dd9b
Pietro Nardelli <pietro.nardelli@outlook.com>
2021-09-21 16:17:58 +0200

    Fixed bug that del every event+runpddl_files works

--- run_pddl_files.py
+++ run_pddl_files.py
@@ -1,20 +1,52 @@
 import os
+from typing import Match, cast
+import re
+import matplotlib.pyplot as plt
 
-base_opt = "base" # or "opt"
+base_opt = "opt" # or "base"
 syn_real = "synthetic" # or "real-life"
-n_constr = ["10", "15", "20"]
+n_constr = ["10"]#, "15", "20"]
 constr_inverted = ["3", "4", "6"]
-len_traces = ["1-50", "51-100", "101-150", "151-200"]
+len_traces = ["1-50", "51-100"]#, "101-150", "151-200"]
+n_traces = 20
 
 for n_cons in n_constr:
     for inv in constr_inverted:
+        cost_tot = []
+        time_tot = []
         for lenght in len_traces:
-            name = n_cons+"_"+inv+"_"+lenght
-            path_domain = "$HOME/fastdownward/PDDL/"+syn_real+"/"+base_opt+"/"+"domain_"+base_opt+"_"+name+".pddl"
-            path_problem = "$HOME/fastdownward/PDDL/"+syn_real+"/"+base_opt+"/"+"problem_"+base_opt+"_"+name+".pddl"
+            cost = 0.0
+            time = 0.0
+            for i in range(n_traces):
+                name = n_cons+"_"+inv+"_"+lenght
+                path_domain = "$HOME/fastdownward/PDDL/"+syn_real+"/"+base_opt+"/"+"domain_"+base_opt+"_"+name+"_trace"+str(i+1)+".pddl"
+                path_problem = "$HOME/fastdownward/PDDL/"+syn_real+"/"+base_opt+"/"+"problem_"+base_opt+"_"+name+"_trace"+str(i+1)+".pddl"
 
-            print(os.system("$HOME/fastdownward/fast-downward.py "+path_domain+" "+path_problem+" --search 'astar(blind())'"))
-
-            break
-        break
-    break
+                output = os.popen("$HOME/fastdownward/fast-downward.py "+path_domain+" "+path_problem+" --search 'astar(blind())'").read()
+                cost += int(re.search("Plan cost: (.*)\n", output).group(1))
+                time += float(re.search("Search time: (.*)s\n", output).group(1))
+        
+            cost_tot.append(cost/n_traces)
+            time_tot.append(time/n_traces)
+        
+        print (cost_tot)
+        print (time_tot)
+    
+        # Plot cost
+        plt.title("5 constraints ("+inv+" modified)")
+        plt.xlabel("Length of the trace")
+        plt.ylabel("Total alignment cost")
+        plt.plot(len_traces, cost_tot, "-o", color="red", label="Cost plot "+n_cons+" "+inv)
+        plt.savefig("results/"+syn_real+"/"+base_opt+"/cost_plot_"+n_cons+"_"+inv+"_part1.png")
+        plt.close()
+        # Plot time
+        plt.title("5 constraints ("+inv+" modified)")
+        plt.xlabel("Length of the trace")
+        plt.ylabel("Total alignment time")
+        plt.plot(len_traces, time_tot, "-o", color="red", label="Time plot "+n_cons+" "+inv)
+        plt.savefig("results/"+syn_real+"/"+base_opt+"/time_plot_"+n_cons+"_"+inv+"_part1.png")
+        plt.close()
+        # Save time and cost in .txt
+        file1 = open("results/"+syn_real+"/"+base_opt+"/"+n_cons+"_"+inv+"_part1.txt", "w")
+        file1.write(str(cost_tot)+"\n"+str(time_tot))
+        file1.close()
